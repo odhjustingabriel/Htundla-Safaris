800ca915361cef232fef607e69969eb035c1c3cc
odhjustingabriel <113585930+odhjustingabriel@users.noreply.github.com>
2026-05-21 22:59:35 +0300

    Document how to access operator dashboard in README

--- htundla_safaris/urls.py
+++ htundla_safaris/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('destinations/', views.destinations, name='destinations'),
     path('contactus/', views.contact_us, name='contactus'),
     path('proposal/send/<int:inquiry_id>/', views.send_proposal, name='send_proposal'),
+    path('operator/dashboard/', views.admin_dashboard, name='admin_dashboard'),
 ]
